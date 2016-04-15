112102e9ab5fe509680aba4e52a769f2982cfb82
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2016-04-15 13:24:03 +0000

    setup webpage

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ var PORT = process.env.PORT;
 var express = require("express");
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
+io.on('connection', function () {
+    console.log("User connected"); 
+});
+
 http.listen(PORT, function() {
     console.log("Server starting on " + PORT);
-    });
+});
