6fa91e80c940da7e362e8561a7f4f7b10da9f723
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2016-04-17 22:52:55 +0000

    add server code

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,8 @@ var socket = io();
 socket.on('connect' , function() {
    console.log('Connected to server'); 
 });
+
+socket.on('message', function(message) {
+   console.log('New message:');
+   console.log(message.text);
+});
