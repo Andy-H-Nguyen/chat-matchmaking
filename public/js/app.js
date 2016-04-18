0694d92762cc4ae52fcf738b243995fe3f4b9c3f
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2016-04-18 03:56:05 +0000

    get basic functionality working

--- public/js/app.js
+++ public/js/app.js
@@ -8,6 +8,8 @@ $( document ).ready(function() {
    socket.on('message', function(message) {
       console.log('New message:');
       console.log(message.text);
+      
+      jQuery('.messages').append('<p>'+ message.text +'</p>');
    });
    
    // Insert Code To Handle Messages
